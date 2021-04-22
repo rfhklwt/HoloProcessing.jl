16e34522910d63970f950a41a6c249de7a5d5d32
Qling <rfhklwt@163.com>
2021-04-22 16:48:08 +0800

    Fix bug

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,19 @@
-using Documenter, HoloProcessing
+# using Documenter, HoloProcessing
 
-format = Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
-makedocs(format = format,
-    sitename="HoloProcessing",
-    pages = [
-            "主页" => "index.md",
-            "手册" => [
-                "标准全息处理" => "man/processing.md",
-                "降噪算法" => "man/denoising.md",
-                "评价指标" => "man/evaluating.md",
-                "函数库" => "man/function.md"]
-    ]
-)
+# format = Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
+# makedocs(format = format,
+#     sitename="HoloProcessing",
+#     pages = [
+#             "主页" => "index.md",
+#             "手册" => [
+#                 "标准全息处理" => "man/processing.md",
+#                 "降噪算法" => "man/denoising.md",
+#                 "评价指标" => "man/evaluating.md",
+#                 "函数库" => "man/function.md"]
+#     ]
+# )
 
-deploydocs(repo = "github.com/rfhklwt/HoloProcessing.jl.git")
+# deploydocs(repo = "github.com/rfhklwt/HoloProcessing.jl.git")
 
 using Documenter, DocumenterLaTeX, HoloProcessing
 include("../contrib/LaTeXWriter.jl")
