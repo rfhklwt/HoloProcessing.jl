ea444c5063aa63c1282cc27168cdc48e9813a878
Qling <41724569+rfhklwt@users.noreply.github.com>
2021-04-22 17:04:19 +0800

    Test (#8)
    * Add Render pdf
    
    * FIx bug
    
    * Fix bug
    
    * modify make.jl
    
    * test

--- docs/make.jl
+++ docs/make.jl
@@ -1,49 +1,16 @@
-# using Documenter, HoloProcessing
+using Documenter, HoloProcessing
 
-# format = Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
-# makedocs(format = format,
-#     sitename="HoloProcessing",
-#     pages = [
-#             "主页" => "index.md",
-#             "手册" => [
-#                 "标准全息处理" => "man/processing.md",
-#                 "降噪算法" => "man/denoising.md",
-#                 "评价指标" => "man/evaluating.md",
-#                 "函数库" => "man/function.md"]
-#     ]
-# )
-
-# deploydocs(repo = "github.com/rfhklwt/HoloProcessing.jl.git")
-
-using Documenter, DocumenterLaTeX, HoloProcessing
-include("../contrib/LaTeXWriter.jl")
-
-const PAGES = [
-    "主页" => "index.md",
-    "手册" => [
-        "标准全息处理" => "man/processing.md",
-        "降噪算法" => "man/denoising.md",
-        "评价指标" => "man/evaluating.md",
-        "函数库" => "man/function.md"],
-]
-
-const render_pdf = "pdf" in ARGS
-
-const format = if render_pdf
-    LaTeX(platform = "texplatform=docker" in ARGS ? "docker" : "native")
-else
-    Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
-end
-
-
-makedocs(
-    format    = format,
-    sitename  = "HoloProcessing中文文档",
-    authors   = "Qling",
-    pages     = PAGES,
+format = Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
+makedocs(format = format,
+    sitename="HoloProcessing",
+    pages = [
+            "主页" => "index.md",
+            "手册" => [
+                "标准全息处理" => "man/processing.md",
+                "降噪算法" => "man/denoising.md",
+                "评价指标" => "man/evaluating.md",
+                "函数库" => "man/function.md"]
+    ]
 )
 
-deploydocs(
-    repo = "github.com/rfhklwt/HoloProcessing.jl.git",
-    branch = render_pdf ? "pdf" : "gh-pages"
-)
+deploydocs(repo = "github.com/rfhklwt/HoloProcessing.jl.git")
